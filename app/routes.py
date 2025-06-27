733e60545011e6f1b204bd72c5d51c4143c3b8cd
Brendon Boechat <boechatbrendon1@gmail.com>
2025-06-27 18:35:00 -0300

    criacao das mensagens ao realizar login com o flash

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,14 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash
 from app.forms import Formlogin
 
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
     form = Formlogin()
-
+    if form.validate_on_submit():
+        if form.usuario.data == 'porteiro' and form.senha.data == '123456':
+            flash('Login realizado com sucesso', 'success')
+        else:
+            flash('Usuario ou Senha, Invalido', 'error')
     return render_template('login.html', form = form)
