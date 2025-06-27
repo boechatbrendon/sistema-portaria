0b8dd690687e94b63efa9d08ce05b8676673841e
Brendon Boechat <boechatbrendon1@gmail.com>
2025-06-27 19:15:42 -0300

    criacao da rota home e a funcionalidade de logar e deslogar

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,33 @@
 from app import app
-from flask import render_template, flash
+from flask import render_template, flash, redirect, url_for
 from app.forms import Formlogin
 from app.models import Usuario
+from flask_login import login_user, login_required, logout_user, current_user
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = Formlogin()
     if form.validate_on_submit():
         usuario = Usuario.query.filter_by(usuario=form.usuario.data).first()
         if usuario and usuario.senha == form.senha.data:
+            login_user(usuario)
             flash('Login realizado com sucesso', 'success')
+            return redirect(url_for('home'))
         else:
             flash('Usuario ou Senha, Invalido', 'error')
     return render_template('login.html', form = form)
+
+
+@app.route('/')
+@login_required
+def home():
+    return render_template('home.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Usuário desconectado.', 'danger')
+    return redirect(url_for('login'))
