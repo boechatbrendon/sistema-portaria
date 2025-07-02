6916ac56625d9ae30f2865eb65b98bd2407e3508
Brendon Boechat <boechatbrendon1@gmail.com>
2025-07-01 21:16:01 -0300

    codigo temporario

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from app import app
-
+from app import app, database
+from app.models import Unidade, Encomenda, Morador, Usuario
+with app.app_context():
+    database.create_all()
+    usuario = Usuario(nome='usuario_test', usuario='Admin', senha='admin')
+    database.session.add(usuario)
+    database.session.commit()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
